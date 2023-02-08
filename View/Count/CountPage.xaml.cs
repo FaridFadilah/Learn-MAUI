d1e733f799ef6aa483d13cadd5e825a8832085e0
farid fadilah permana <faridfadillah29@gmail.com>
2023-02-08 22:44:43 +0700

    make Kotak

--- View/Count/CountPage.xaml.cs
+++ View/Count/CountPage.xaml.cs
@@ -4,12 +4,22 @@ public partial class CountPage : ContentPage{
   int count = 0;
 	public CountPage(){
 		InitializeComponent();
+    countLabel.Text = $"Hitungan anda {count}";
 	}
   
   private void countPlus(object sender, EventArgs e){
     count++;
-    if(count == 1) countButton.Text = $"{count}";
-    else countButton.Text = $"{count}";
+    if(count >= 0) countLabel.Text = $"hitungan {count}";
+  }
+
+  async void countMinus(object sender, EventArgs e){
     
+    count--;
+    if(count <= 0){
+      count = 0;
+      countLabel.Text = $"Hitungan anda {count}";
+      await DisplayAlert("msg", "hitungan mu kurang dari 0", "OK");
+    }
+    else countLabel.Text = $"hitungan {count}";
   }
 }
